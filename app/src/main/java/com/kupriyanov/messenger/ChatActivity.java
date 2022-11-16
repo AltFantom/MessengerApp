d953c9dbbe2bf104c4715dbc34c1dc7f747467cf
AltF4ntom <AltF4ntom@yandex.ru>
2022-11-16 17:32:07 +0300

    ChatActivity

---
+++ app/src/main/java/com/kupriyanov/messenger/ChatActivity.java
@@ -0,0 +1,33 @@
+package com.kupriyanov.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private View viewOnlineStatus;
+    private RecyclerView recyclerViewMessages;
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+    private void initViews() {
+        textViewTitle = findViewById(R.id.textViewTitle);
+        viewOnlineStatus = findViewById(R.id.viewOnlineStatus);
+        recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+}
