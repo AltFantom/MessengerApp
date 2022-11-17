3878f2ea0a1a5ef7660c03ceb057063129660aa1
AltF4ntom <AltF4ntom@yandex.ru>
2022-11-17 12:54:43 +0300

    ChatViewModelFactory and observeViewModel(), setupClickListeners() in ChatActivity

--- app/src/main/java/com/kupriyanov/messenger/ChatActivity.java
+++ app/src/main/java/com/kupriyanov/messenger/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.kupriyanov.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +33,9 @@ public class ChatActivity extends AppCompatActivity {
     private String currentUserId;
     private String otherUserId;
 
+    private ChatViewModel viewModel;
+    private ChatViewModelFactory viewModelFactory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,27 +43,68 @@ public class ChatActivity extends AppCompatActivity {
         initViews();
         currentUserId = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
         otherUserId = getIntent().getStringExtra(EXTRA_OTHER_USER_ID);
+        viewModelFactory = new ChatViewModelFactory(currentUserId, otherUserId);
+        viewModel = new ViewModelProvider(this, viewModelFactory).get(ChatViewModel.class);
         messagesAdapter = new MessagesAdapter(currentUserId);
         recyclerViewMessages.setAdapter(messagesAdapter);
+        observeViewModel();
+        setupClickListeners();
+    }
 
-        List<Message> messages = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Message message = new Message(
-                    "Text" + i,
-                    currentUserId,
-                    otherUserId
-            );
-            messages.add(message);
-        }
-        for (int i = 0; i < 10; i++) {
-            Message message = new Message(
-                    "Text" + i,
-                    otherUserId,
-                    currentUserId
-            );
-            messages.add(message);
-        }
-        messagesAdapter.setMessages(messages);
+    private void setupClickListeners() {
+        imageViewSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textMessage = editTextMessage.getText().toString().trim();
+                if (textMessage.equals("")) {
+                    return;
+                }
+                Message message = new Message(
+                        textMessage,
+                        currentUserId,
+                        otherUserId
+                );
+                viewModel.sendMessage(message);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getMessages().observe(this, new Observer<List<Message>>() {
+            @Override
+            public void onChanged(List<Message> messages) {
+                messagesAdapter.setMessages(messages);
+            }
+        });
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(
+                            ChatActivity.this,
+                            errorMessage,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        });
+        viewModel.getMessageSent().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean sent) {
+                if (sent) {
+                    editTextMessage.setText("");
+                }
+            }
+        });
+        viewModel.getOtherUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if (user != null) {
+                    String userInfo = String.format("%s %s", user.getName(), user.getLastName());
+                    textViewTitle.setText(userInfo);
+                }
+            }
+        });
     }
 
     private void initViews() {
